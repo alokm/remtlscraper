e66285e6ef38ed5ab7489bc8c61a92b26f878860
Alok Mohindra <alok.mohindra@gmail.com>
2011-12-24 12:56:04 -0500

    updated remtlscraper.py to parse command line arguments per MVP spec in README

--- src/remtlscraper/remtlscraper.py
+++ src/remtlscraper/remtlscraper.py
@@ -2,27 +2,18 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-desc="Command line utility for downloading documentation on air, water, and soil contamination data from public websites")
+title="Command line utility for downloading pdf docs on contamination data"
 
-parser = argparse.ArgumentParser(description=desc)
-parser.add_argument('-g', '--get', action='store', metavar='NN',
+parser = argparse.ArgumentParser(description=title)
+parser.add_argument('-g', action='store', metavar='MRC',
                     dest='mrc_code', type=int, nargs ='+',
                     help='MRC code for region or municipality')
 parser.add_argument('-l', '--list', action='store_true', default=False,
                     dest='listflag',
                     help='list available MRC codes')
-parser.add_argument('-u', '--update', action='store_true', default=False,
-                    dest='updateflag',
-                    help='update list of available MRC codes')
-parser.add_argument('-a', '--all', action='store_true', default=False,
-                    dest='downloadall',
-                    help='download ALL source files for ALL available MRC' +
-                         'codes. WARNING: This will download a LOT of data')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0',
                     help='last updated 2011-12-23 by Alok Mohindra')
 
 results = parser.parse_args()
-print 'mrc_code   =', results.mrc_code
+print 'mrc_codes   =', results.mrc_code
 print 'list   =', results.listflag
-print 'update   =', results.updateflag
-print 'all =', results.downloadall
