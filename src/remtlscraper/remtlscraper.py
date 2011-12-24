3ac1a9e7f2e79c128dd3b8ac2cda084fa62559dd
Alok Mohindra <alok.mohindra@gmail.com>
2011-12-23 23:53:27 -0500

    updated remtlscraper.py to parse command line arguments with argparse

--- src/remtlscraper/remtlscraper.py
+++ src/remtlscraper/remtlscraper.py
@@ -1,45 +1,27 @@
-#!/usr/bin/python
+import argparse
 
-from urllib import urlopen
-from BeautifulSoup import BeautifulSoup as soup
-#from lxml import etree
+parser = argparse.ArgumentParser()
 
+parser = argparse.ArgumentParser(description="Command line utility for downloading documentation on air, water, and soil contamination data from public websites")
+parser.add_argument('-g', '--get', action='store', metavar='NN',
+                    dest='mrc_code', type=int, nargs ='+',
+                    help='MRC code for region or municipality')
+parser.add_argument('-l', '--list', action='store_true', default=False,
+                    dest='listflag',
+                    help='list available MRC codes')
+parser.add_argument('-u', '--update', action='store_true', default=False,
+                    dest='updateflag',
+                    help='update list of available MRC codes')
+parser.add_argument('-a', '--all', action='store_true', default=False,
+                    dest='downloadall',
+                    help='download ALL source files for ALL available MRC' +
+                         'codes. WARNING: This will download a LOT of data')
+parser.add_argument('--version', action='version', version='%(prog)s 1.0',
+                    help='last updated 2011-12-23 by Alok Mohindra')
 
-class BaseScraper(object):
-    help = 'Command line utility to mine data for ReMtl.ca'
-
-    def __init__(self):
-        self.url = "http://www.mddep.gouv.qc.ca/sol/terrains/terrains-contamines/recherche.asp"
-        self.page = urlopen(self.url)
-        self.text = self.page.read()
-        self.doc = soup(self.text)
-        self.form = self.doc.body.find('form')
-        self.table = self.form.find('table')
-        self.rows = self.table.findAll('tr') 
-        self.muni = self.rows[3]
-        self.mrc = self.rows[4]
-        self.cont = self.rows[5]
-
-    def update_codes(self):
-        print "Retrieving Municipality Codes"
-
-        municipalite = dict()
-        for item in self.muni.findAll('option'):
-            if item.text == 'Tous':
-                print '-' * 40
-                print u'Code| Municipalite'
-                print '-' * 40
-            else:
-                municipalite[item.attrs[0][1]] = item.text
-                print item.attrs[0][1] + ' | ' + item.text
-    
-        # next step: modify update_codes method to return munis
-        # as dict() with codes as keys and muni name as value
-        
-        # To do: add mrc and contaminant updaters
-        #   mrc = dict()
-        #   contaminant = dict()
-
-if '__name__' == '__main__':
-    scraper = BaseScraper()
-    scraper.update_codes()
+results = parser.parse_args()
+print "-" * 24
+print 'mrc_code   =', results.mrc_code
+print 'list   =', results.listflag
+print 'update   =', results.updateflag
+print 'all =', results.downloadall
